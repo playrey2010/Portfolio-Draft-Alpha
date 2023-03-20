14955cedd2a4176d3eb4e29286e122e8f5959977
playrey2010 <reynaldo.emanuel@gmail.com>
2023-03-20 10:01:09 -0700

    added switch sections logic to the div.sections

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,15 +3,38 @@ const sectionButtons = document.querySelectorAll('.controls')
 const sectionButton = document.querySelectorAll('.control')
 const allSection = document.querySelectorAll('.main-content');
 
-function pageTranistions(){
+function pageTransitions(){
     //button click active class
     for(let i = 0; i < sectionButton.length; i++) {
         sectionButton[i].addEventListener('click', function () {
-            let currentBtn = document.querySelectorAll('.active-btn');
+            // find the button that currently has 'active-btn' class.
+            let currentBtn = document.querySelectorAll('.active-btn'); // querySelector creates array
+            // and there is only one possible result (the current button), so it defaults to index 0.
             currentBtn[0].className = currentBtn[0].className.replace(' active-btn', '');
-            this.className += ' active-btn'
+            this.className += ' active-btn';
         } )
     }
+
+    // sections active class
+    allSection[0].addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+            // remove 'selected' from the other buttons
+            sectionButtons.forEach((btn) =>{
+                btn.classList.remove('active');
+            })
+            e.target.classList.add('active');
+
+            // hide other sections
+            sections.forEach((section)=> {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+
+    } )
 }
 
-pageTranistions()
+pageTransitions()

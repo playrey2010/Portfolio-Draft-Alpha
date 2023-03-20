22e32a5932b4c18d1393cbea7ba89e3280236436
playrey2010 <reynaldo.emanuel@gmail.com>
2023-03-20 15:10:34 -0700

    brought over default files

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,40 +1,13 @@
-const sections = document.querySelectorAll('.section');
-const sectionButtons = document.querySelectorAll('.controls')
-const sectionButton = document.querySelectorAll('.control')
-const allSection = document.querySelector('.main-content');
-
-function pageTransitions(){
-    //button click active class
-    for(let i = 0; i < sectionButton.length; i++) {
-        sectionButton[i].addEventListener('click', function () {
-            // find the button that currently has 'active-btn' class.
-            let currentBtn = document.querySelectorAll('.active-btn'); // querySelector creates array
-            // and there is only one possible result (the current button), so it defaults to index 0.
-            currentBtn[0].className = currentBtn[0].className.replace(' active-btn', '');
-            this.className += ' active-btn';
-        } )
-    }
-
-    // sections active class
-    allSection.addEventListener('click', (e) => {
-        const id = e.target.dataset.id;
-        if (id) {
-            // remove 'selected' from the other buttons
-            // sectionButtons.forEach((btn) =>{
-            //     btn.classList.remove('active');
-            // })
-            // e.target.classList.add('active');
-
-            // hide other sections
-            sections.forEach((section)=> {
-                section.classList.remove('active')
-            })
-
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        }
-
-    } )
-}
-
-pageTransitions()
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
